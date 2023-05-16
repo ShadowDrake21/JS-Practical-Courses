3f286e85d654844bad6471261111de3fbb7037af
Dmytro <122775021+ShadowDrake21@users.noreply.github.com>
2023-05-16 09:15:49 +0300

    004 Работа с табами (вкладками) на странице
    [Иван Петриченко] Практический JavaScript (2020). 01 Проект №1. Классический стиль

--- real1/js/main.js
+++ real1/js/main.js
@@ -1,5 +1,12 @@
 window.addEventListener('DOMContentLoaded', () => {
   modals()
+  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active')
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  )
 })
 
 // modals
@@ -51,7 +58,51 @@ const modals = () => {
 
   bindModal('.phone_link', '.popup', '.popup .popup_close')
 
-  showModalByTime('.popup', 3000)
+  // showModalByTime('.popup', 60000)
 }
 
 // modals
+
+// tabs
+
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
+  const header = document.querySelector(headerSelector),
+    tab = document.querySelectorAll(tabSelector),
+    content = document.querySelectorAll(contentSelector)
+
+  function hideTabContent() {
+    content.forEach((item) => {
+      item.style.display = 'none'
+    })
+
+    tab.forEach((item) => {
+      item.classList.remove(activeClass)
+    })
+  }
+
+  function showTabContent(i = 0) {
+    content[i].style.display = 'block'
+    tab[i].classList.add(activeClass)
+  }
+
+  hideTabContent()
+  showTabContent()
+
+  header.addEventListener('click', (e) => {
+    const target = e.target
+    if (
+      target &&
+      (target.classList.contains(tabSelector.replace(/\./, '')) ||
+        target.parentNode.classList.contains(tabSelector.replace(/\./, '')))
+    ) {
+      tab.forEach((item, i) => {
+        if (target === item || target.parentNode === item) {
+          hideTabContent()
+          showTabContent(i)
+        }
+      })
+    }
+  })
+}
+
+// tabs
